0c478bab80383d80149684e0d07d555d1c011762
Zumrut-Vie <ekinci.z@gmail.com>
2020-05-25 13:04:06 +0200

    advanced ex

--- doc.js
+++ doc.js
@@ -31,3 +31,48 @@ console.log(animals[0]);
 console.log(animals[1]);
 console.log(animals[2]);
 console.log(animals[3]);
+
+// ??????????
+var fruities = ["mango","cherries","kiwi","grapes","pear","peach","orange","lemon"];
+document.write(fruities.join("\n"));
+
+//Advanced Ex 1
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+
+var n = a + b + number(c) + number(d) + e;
+console.log(n);
+
+var f = '1';
+var g = 15;
+var h = 8;
+var i= "1";
+
+var z = number(f)*g*h*number(i);
+console.log(z);
+
+var result = n/z;
+document.write(result);
+
+//Advanced Ex 2
+
+numbersArr [ 
+[1, 2, 1, 24], 
+[8, 11, 9, 4], 
+[7, 0, 7, 27], 
+[7, 4, 28, 14], 
+[3, 10, 25, 7], 
+[21, 4, 6, 17], 
+[3, 5, 26, 3] 
+];
+
+console.log(numbersArr[1][1]);
+console.log(numbersArr[4][2]);
+console.log(numbersArr[5][3]);
+console.log(numbersArr[2][3]);
+console.log(numbersArr[2][1]);
+
+
