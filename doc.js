fee48caa9edf68e76f1caeb7d348afa9311464b9
Ruben <wsurfing.ruben@mail.com>
2020-05-25 14:31:16 +0200

    practice

--- doc.js
+++ doc.js
@@ -33,7 +33,7 @@ console.log(animals[2]);
 console.log(animals[3]);
 
 // ??????????
-var fruities = ["mango","cherries","kiwi","grapes","pear","peach","orange","lemon"];
+var fruities = ["mango", "cherries", "kiwi", "grapes", "pear", "peach", "orange", "lemon"];
 document.write(fruities.join("\n"));
 
 //Advanced Ex 1
@@ -43,36 +43,34 @@ var c = "21";
 var d = '36';
 var e = 42;
 
-var n = a + b + number(c) + number(d) + e;
+var n = 7 + 14 + Number("21") + Number("36") + 42;
 console.log(n);
 
 var f = '1';
 var g = 15;
 var h = 8;
-var i= "1";
+var i = "1";
 
-var z = number(f)*g*h*number(i);
+var z = Number('1') * g * h * Number("1");
 console.log(z);
 
-var result = n/z;
+var result = n / z;
 document.write(result);
 
 //Advanced Ex 2
 
-numbersArr [ 
-[1, 2, 1, 24], 
-[8, 11, 9, 4], 
-[7, 0, 7, 27], 
-[7, 4, 28, 14], 
-[3, 10, 25, 7], 
-[21, 4, 6, 17], 
-[3, 5, 26, 3] 
+numbersArr = [
+    [1, 2, 1, 24],
+    [8, 11, 9, 4],
+    [7, 0, 7, 27],
+    [7, 4, 28, 14],
+    [3, 10, 25, 7],
+    [21, 4, 6, 17],
+    [3, 5, 26, 3]
 ];
 
-console.log(numbersArr[1][1]);
-console.log(numbersArr[4][2]);
-console.log(numbersArr[5][3]);
-console.log(numbersArr[2][3]);
-console.log(numbersArr[2][1]);
+console.log(numbersArr[1][1], numbersArr[4][2], numbersArr[5][3], numbersArr[2][3], numbersArr[2][1]);
 
-
+var sentence = ("Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up")
+sentence = sentence.replaceAll("[$]", " ");
+console.log(sentence);
