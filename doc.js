b9f885a2280afdc804a9f9f3ca8453e5156ff873
Ruben <wsurfing.ruben@mail.com>
2020-05-25 11:19:45 +0200

    edited js files

--- doc.js
+++ doc.js
@@ -7,3 +7,27 @@ console.log("Hi my name is Martin","and I am" +age+ "years old");*/
 players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
 console.log(players[2]);
 
+
+cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat",
+    "Ferrari"
+];
+console.log(cars.sort());
+
+//Intermediate Excercise 2
+
+fruits = ["apple", "banana", "kiwi", ];
+fruits.push("orange");
+console.log(fruits);
+console.log(fruits.pop());
+console.log(fruits);
+
+animals = ["monkey", "horse", "dog"];
+animals.reverse();
+console.log(animals);
+animals.unshift("cat");
+console.log(animals);
+// other trick for new line of each array item
+console.log(animals[0]);
+console.log(animals[1]);
+console.log(animals[2]);
+console.log(animals[3]);
