3ca43be572445889d3a58bb56172bb5fd3376d64
Zumrut-Vie <ekinci.z@gmail.com>
2020-05-25 10:34:32 +0200

    JS Day 1

---
+++ doc.js
@@ -0,0 +1,9 @@
+/*console.log("Hi my name is Martin")
+
+var age= 25;
+
+console.log("Hi my name is Martin","and I am" +age+ "years old");*/
+
+players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+console.log(players[2]);
+
